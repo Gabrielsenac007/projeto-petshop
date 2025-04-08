1779bb932711fce8234d00e16fd5f4ee4103ccc3
Gabriel <gabrielvdcsilva@gmail.com>
2025-04-08 16:37:50 -0300

    added workflows

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 // src/server.ts
 import app from './app';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT!;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
